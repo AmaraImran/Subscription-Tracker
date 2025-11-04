f1d5350445115ca814d0622e5c99b1cfa69a2e89
AmaraImran <khanamara265@gmail.com>
2025-11-04 12:19:11 +0500

    removed error

--- subscriptionfrontend/src/App.jsx
+++ subscriptionfrontend/src/App.jsx
@@ -7,12 +7,13 @@ import AddSubscription from './components/subscription/Add-subscription'
 import UpdateSubscription from './components/subscription/Update-subscription'
 import UserSubscriptions from './components/subscription/ShowAllsubscriptions'
 import NotFound from './components/Pagenotfound'
+import SignupPage from './components/auth/Sign-up'
 const App = () => {
   return (
 
     <Routes>
       <Route path='/' element={<LoginPage/>}/>
-      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/signup' element={<SignupPage/>}/>
       <Route path='/dashboard' element={<Dashboard/>}/>
       <Route path='/add-subscription' element={<AddSubscription/>}/>
       <Route path='/update-subscription/:id' element={<UpdateSubscription/>}/>
